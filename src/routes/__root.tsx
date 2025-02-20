6518627698a8891e14679d207d0424bb8ebff420
Venchhut <ijinbaekk@gmail.com>
2025-02-20 12:21:21 +0700

    fix: refactor ui

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,36 +1,56 @@
-import * as React from "react";
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { Container, Title, Group, Button, AppShell, rem } from "@mantine/core";
+
+const navLinks = [
+  { title: "Home", to: "/" },
+  { title: "About", to: "/about" },
+  { title: "Profile", to: "/profile" },
+  { title: "Pokemon", to: "/pokemon" },
+];
+
+const activeProps = {
+  style: {
+    backgroundColor: "#228be6",
+    color: "white",
+  },
+};
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
-const activeProps = {
-  style: {
-    color: "red",
-  },
-};
+
 function RootComponent() {
   return (
-    <React.Fragment>
-      <h1>Root</h1>
-      <ul>
-        <li>
-          <Link to="/about" activeProps={activeProps}>
-            about
-          </Link>
-        </li>
-        <li>
-          <Link to="/profile" activeProps={activeProps}>
-            Profile
-          </Link>
-        </li>
-        <li>
-          <Link to="/pokemon/" activeProps={activeProps}>
-            Pokemon
-          </Link>
-        </li>
-      </ul>
-      <Outlet />
-    </React.Fragment>
+    <AppShell header={{ height: 60 }} padding="md">
+      <AppShell.Header>
+        <Container size="xl" h="100%">
+          <Group justify="space-between" h="100%" px="md">
+            <Title order={2} c="blue.6">
+              TanStack Query
+            </Title>
+            <Group gap="sm">
+              {navLinks.map((link) => (
+                <Button
+                  key={link.title}
+                  component={Link}
+                  to={link.to}
+                  variant="subtle"
+                  size="sm"
+                  activeProps={activeProps}
+                >
+                  {link.title}
+                </Button>
+              ))}
+            </Group>
+          </Group>
+        </Container>
+      </AppShell.Header>
+
+      <AppShell.Main>
+        <Container size="xl" py={rem(20)}>
+          <Outlet />
+        </Container>
+      </AppShell.Main>
+    </AppShell>
   );
 }
